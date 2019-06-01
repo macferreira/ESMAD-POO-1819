53df989a9e059dec97479adb645ac9d3284e5fa1
Miguel Ferreira <miguel.correiaferreira@gmail.com>
2019-06-01 19:48:40 +0100

    Add teste01-extra ex2a resolution

---
+++ teste01-extra/ex2a.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("form01");
+const div = document.getElementById("div01");
+const buttonDeleteLast = document.getElementById("delete-last-btn");
+const buttonDeleteAll = document.getElementById("delete-all-btn");
+
+let proverbs = [];
+
+function renderProverbsDiv() {
+    if (proverbs.length === 0) {
+
+        div.innerHTML = "<p>SEM PROVÉRBIOS</p>";
+    } else {
+        div.innerHTML = "";
+
+        for (i = proverbs.length-1; i >= 0 ; i--) {
+
+            let currentParagraph = document.createElement("p");
+            let currentProverb = document.createTextNode(proverbs[i]);
+            
+            currentParagraph.appendChild(currentProverb);
+            div.appendChild(currentParagraph);
+        }
+    }
+
+    console.log(proverbs);
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let proverb = document.getElementById("proverb");
+
+    proverbs.push(proverb.value);
+
+    renderProverbsDiv();
+
+    proverb.value = "";
+});
+
+buttonDeleteLast.addEventListener("click", function() {
+    proverbs.pop();
+    renderProverbsDiv();
+});
+
+buttonDeleteAll.addEventListener("click", function() {
+    proverbs = [];
+    renderProverbsDiv();
+});
